1baee37b1fc1bec5810b8344d691d570c6cb4158
Jakub <jkbpawlowski@student.agh.edu.pl>
2023-03-27 03:44:03 +0200

    1

--- jonhson.py
+++ jonhson.py
@@ -2,7 +2,7 @@ from math import inf
 from typing import Dict, List
 from get_nodes import *
 from graphs import *
-from bellmanford import *
+
 
 V, w, G = get_nodes_from_list(G)
 print(V)
@@ -10,3 +10,13 @@ print('\n')
 print(w)
 print('\n')
 print(G)
+
+
+def Johnson(G, type, s):
+    n = len(G)
+    if type == 'matrix':
+        V, w, G = get_nodes_from_matrix(G)
+    elif type == 'list':
+        V, w, G = get_nodes_from_list(G)
+    return V, w, G
+print(Johnson(G, 'list', 0))
